a90ada5929af544304b334a7dd25a769539964dd
GCRA101 <giorgio.albieri@burohappold.com>
2024-05-01 20:57:34 +0100

    Add Splashscreen and AboutBox

---
+++ view/aboutBox.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class aboutBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    Friend WithEvents TableLayoutPanel As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents lblProductName As System.Windows.Forms.Label
+    Friend WithEvents lblVersion As System.Windows.Forms.Label
+    Friend WithEvents lblCompanyName As System.Windows.Forms.Label
+    Friend WithEvents OKButton As System.Windows.Forms.Button
+    Friend WithEvents lblCopyRight As System.Windows.Forms.Label
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(aboutBox))
+        Me.TableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblProductName = New System.Windows.Forms.Label()
+        Me.lblVersion = New System.Windows.Forms.Label()
+        Me.lblCopyRight = New System.Windows.Forms.Label()
+        Me.lblCompanyName = New System.Windows.Forms.Label()
+        Me.OKButton = New System.Windows.Forms.Button()
+        Me.panelImage = New System.Windows.Forms.Panel()
+        Me.txtDescription = New System.Windows.Forms.RichTextBox()
+        Me.TableLayoutPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel
+        '
+        Me.TableLayoutPanel.ColumnCount = 2
+        Me.TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.0!))
+        Me.TableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.0!))
+        Me.TableLayoutPanel.Controls.Add(Me.lblProductName, 1, 0)
+        Me.TableLayoutPanel.Controls.Add(Me.lblVersion, 1, 1)
+        Me.TableLayoutPanel.Controls.Add(Me.lblCopyRight, 1, 2)
+        Me.TableLayoutPanel.Controls.Add(Me.lblCompanyName, 1, 3)
+        Me.TableLayoutPanel.Controls.Add(Me.OKButton, 1, 5)
+        Me.TableLayoutPanel.Controls.Add(Me.panelImage, 0, 0)
+        Me.TableLayoutPanel.Controls.Add(Me.txtDescription, 0, 4)
+        Me.TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel.Location = New System.Drawing.Point(9, 9)
+        Me.TableLayoutPanel.Name = "TableLayoutPanel"
+        Me.TableLayoutPanel.RowCount = 6
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel.Size = New System.Drawing.Size(396, 323)
+        Me.TableLayoutPanel.TabIndex = 0
+        '
+        'lblProductName
+        '
+        Me.lblProductName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblProductName.Location = New System.Drawing.Point(136, 0)
+        Me.lblProductName.Margin = New System.Windows.Forms.Padding(6, 0, 3, 0)
+        Me.lblProductName.MaximumSize = New System.Drawing.Size(0, 17)
+        Me.lblProductName.Name = "lblProductName"
+        Me.lblProductName.Size = New System.Drawing.Size(257, 17)
+        Me.lblProductName.TabIndex = 0
+        Me.lblProductName.Text = "Product Name"
+        Me.lblProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblVersion
+        '
+        Me.lblVersion.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblVersion.Location = New System.Drawing.Point(136, 32)
+        Me.lblVersion.Margin = New System.Windows.Forms.Padding(6, 0, 3, 0)
+        Me.lblVersion.MaximumSize = New System.Drawing.Size(0, 17)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(257, 17)
+        Me.lblVersion.TabIndex = 0
+        Me.lblVersion.Text = "Version"
+        Me.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblCopyRight
+        '
+        Me.lblCopyRight.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblCopyRight.Location = New System.Drawing.Point(136, 64)
+        Me.lblCopyRight.Margin = New System.Windows.Forms.Padding(6, 0, 3, 0)
+        Me.lblCopyRight.MaximumSize = New System.Drawing.Size(0, 17)
+        Me.lblCopyRight.Name = "lblCopyRight"
+        Me.lblCopyRight.Size = New System.Drawing.Size(257, 17)
+        Me.lblCopyRight.TabIndex = 0
+        Me.lblCopyRight.Text = "Copyright"
+        Me.lblCopyRight.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblCompanyName
+        '
+        Me.lblCompanyName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblCompanyName.Location = New System.Drawing.Point(136, 96)
+        Me.lblCompanyName.Margin = New System.Windows.Forms.Padding(6, 0, 3, 0)
+        Me.lblCompanyName.MaximumSize = New System.Drawing.Size(0, 17)
+        Me.lblCompanyName.Name = "lblCompanyName"
+        Me.lblCompanyName.Size = New System.Drawing.Size(257, 17)
+        Me.lblCompanyName.TabIndex = 0
+        Me.lblCompanyName.Text = "Company Name"
+        Me.lblCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'OKButton
+        '
+        Me.OKButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.OKButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.OKButton.Location = New System.Drawing.Point(318, 297)
+        Me.OKButton.Name = "OKButton"
+        Me.OKButton.Size = New System.Drawing.Size(75, 23)
+        Me.OKButton.TabIndex = 0
+        Me.OKButton.Text = "&OK"
+        '
+        'panelImage
+        '
+        Me.panelImage.BackgroundImage = CType(resources.GetObject("panelImage.BackgroundImage"), System.Drawing.Image)
+        Me.panelImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.panelImage.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelImage.Location = New System.Drawing.Point(3, 3)
+        Me.panelImage.Name = "panelImage"
+        Me.TableLayoutPanel.SetRowSpan(Me.panelImage, 4)
+        Me.panelImage.Size = New System.Drawing.Size(124, 122)
+        Me.panelImage.TabIndex = 1
+        '
+        'txtDescription
+        '
+        Me.TableLayoutPanel.SetColumnSpan(Me.txtDescription, 2)
+        Me.txtDescription.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtDescription.Location = New System.Drawing.Point(3, 131)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(390, 155)
+        Me.txtDescription.TabIndex = 2
+        Me.txtDescription.Text = ""
+        '
+        'aboutBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.OKButton
+        Me.ClientSize = New System.Drawing.Size(414, 341)
+        Me.Controls.Add(Me.TableLayoutPanel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "aboutBox"
+        Me.Padding = New System.Windows.Forms.Padding(9)
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "AboutBox"
+        Me.TableLayoutPanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panelImage As Windows.Forms.Panel
+    Friend WithEvents txtDescription As Windows.Forms.RichTextBox
+End Class
---
+++ view/splashScreen.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class splashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(splashScreen))
+        Me.pbAppLogo = New System.Windows.Forms.PictureBox()
+        Me.tlpLogo = New System.Windows.Forms.TableLayoutPanel()
+        Me.tlpTitle = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblApplicationName = New System.Windows.Forms.Label()
+        Me.lblVersion = New System.Windows.Forms.Label()
+        Me.lblCopyright = New System.Windows.Forms.Label()
+        CType(Me.pbAppLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.tlpLogo.SuspendLayout()
+        Me.tlpTitle.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pbAppLogo
+        '
+        Me.pbAppLogo.BackgroundImage = CType(resources.GetObject("pbAppLogo.BackgroundImage"), System.Drawing.Image)
+        Me.pbAppLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.pbAppLogo.Location = New System.Drawing.Point(3, 3)
+        Me.pbAppLogo.Name = "pbAppLogo"
+        Me.pbAppLogo.Size = New System.Drawing.Size(316, 298)
+        Me.pbAppLogo.TabIndex = 0
+        Me.pbAppLogo.TabStop = False
+        '
+        'tlpLogo
+        '
+        Me.tlpLogo.ColumnCount = 2
+        Me.tlpLogo.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.98994!))
+        Me.tlpLogo.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.01006!))
+        Me.tlpLogo.Controls.Add(Me.pbAppLogo, 0, 0)
+        Me.tlpLogo.Controls.Add(Me.tlpTitle, 1, 0)
+        Me.tlpLogo.Location = New System.Drawing.Point(1, 0)
+        Me.tlpLogo.Name = "tlpLogo"
+        Me.tlpLogo.RowCount = 1
+        Me.tlpLogo.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpLogo.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpLogo.Size = New System.Drawing.Size(497, 304)
+        Me.tlpLogo.TabIndex = 1
+        '
+        'tlpTitle
+        '
+        Me.tlpTitle.ColumnCount = 1
+        Me.tlpTitle.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpTitle.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpTitle.Controls.Add(Me.lblApplicationName, 0, 0)
+        Me.tlpTitle.Controls.Add(Me.lblVersion, 0, 1)
+        Me.tlpTitle.Controls.Add(Me.lblCopyright, 0, 2)
+        Me.tlpTitle.Location = New System.Drawing.Point(325, 3)
+        Me.tlpTitle.Name = "tlpTitle"
+        Me.tlpTitle.RowCount = 3
+        Me.tlpTitle.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.99522!))
+        Me.tlpTitle.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.00478!))
+        Me.tlpTitle.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 88.0!))
+        Me.tlpTitle.Size = New System.Drawing.Size(169, 298)
+        Me.tlpTitle.TabIndex = 1
+        '
+        'lblApplicationName
+        '
+        Me.lblApplicationName.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblApplicationName.AutoSize = True
+        Me.lblApplicationName.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblApplicationName.Location = New System.Drawing.Point(3, 122)
+        Me.lblApplicationName.Name = "lblApplicationName"
+        Me.lblApplicationName.Size = New System.Drawing.Size(144, 64)
+        Me.lblApplicationName.TabIndex = 0
+        Me.lblApplicationName.Text = "Application Name"
+        '
+        'lblVersion
+        '
+        Me.lblVersion.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblVersion.AutoSize = True
+        Me.lblVersion.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblVersion.Location = New System.Drawing.Point(54, 186)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(112, 20)
+        Me.lblVersion.TabIndex = 1
+        Me.lblVersion.Text = "Version Number"
+        '
+        'lblCopyright
+        '
+        Me.lblCopyright.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblCopyright.AutoSize = True
+        Me.lblCopyright.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCopyright.Location = New System.Drawing.Point(107, 209)
+        Me.lblCopyright.Name = "lblCopyright"
+        Me.lblCopyright.Size = New System.Drawing.Size(59, 15)
+        Me.lblCopyright.TabIndex = 2
+        Me.lblCopyright.Text = "Copyright"
+        '
+        'splashScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(496, 303)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.tlpLogo)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "splashScreen"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.pbAppLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.tlpLogo.ResumeLayout(False)
+        Me.tlpTitle.ResumeLayout(False)
+        Me.tlpTitle.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbAppLogo As Windows.Forms.PictureBox
+    Friend WithEvents tlpLogo As Windows.Forms.TableLayoutPanel
+    Friend WithEvents tlpTitle As Windows.Forms.TableLayoutPanel
+    Friend WithEvents lblApplicationName As Windows.Forms.Label
+    Friend WithEvents lblVersion As Windows.Forms.Label
+    Friend WithEvents lblCopyright As Windows.Forms.Label
+End Class
