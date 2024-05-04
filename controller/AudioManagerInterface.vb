bdd6ad7fbad62c420b56869b6ee07a9db893fbe9
GCRA101 <giorgio.albieri@burohappold.com>
2024-05-04 16:26:27 +0100

    Add AudioManager

---
+++ controller/AudioManagerInterface.vb
@@ -0,0 +1,13 @@
+﻿Public Interface AudioManagerInterface
+
+    'AUDIOMANAGERINTERFACE
+
+    'interface that defines the methods that all the concrete classes of 
+    'type AudioManager have to implement.
+
+    Sub play(fileName As String)
+    Sub setActive(bool As Boolean)
+    Function isActive() As Boolean
+
+
+End Interface
---
+++ controller/SoundManager.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+Imports System.Reflection.Emit
+
+Public Class SoundManager
+	Implements AudioManagerInterface
+
+	' SOUNDSMANAGER
+	' 
+	' Concrete Class implementing the interface AudioManagerInterface and that is responsible 
+	' for playing the sound effects of the application, when active.
+	'
+	' Specific Techniques
+	' - SINGLETON Design Pattern
+
+	' ATTRIBUTES
+	Private Shared instance As SoundManager
+	Private active As Boolean = True
+
+	' CONSTRUCTOR - Private'
+	Private Sub New()
+	End Sub
+
+	' STATIC METHOD .getInstance() '
+	Public Shared Function getInstance() As SoundManager
+		If (instance Is Nothing) Then
+			instance = New SoundManager()
+		End If
+		Return instance
+	End Function
+
+	Public Sub play(filePath As String) Implements AudioManagerInterface.play
+		'Execute this Sub only if the SoundManager is set to active
+		If Me.active = False Then Return
+
+		Try
+			'Play the File in the background without stopping user's interaction
+			My.Computer.Audio.Play(filePath, AudioPlayMode.Background)
+		Catch ex1 As FileNotFoundException
+			MsgBox(ex1.Message, vbOKOnly And MsgBoxStyle.Exclamation, "WARNING")
+		Catch ex2 As IOException
+			MsgBox(ex2.Message, vbOKOnly And MsgBoxStyle.Exclamation, "WARNING")
+		Catch ex3 As Exception
+			MsgBox(ex3.Message, vbOKOnly And MsgBoxStyle.Exclamation, "WARNING")
+		End Try
+
+	End Sub
+
+	Public Sub setActive(active As Boolean) Implements AudioManagerInterface.setActive
+		Me.active = active
+	End Sub
+
+	Public Function isActive() As Boolean Implements AudioManagerInterface.isActive
+		Return Me.active
+	End Function
+
+End Class
